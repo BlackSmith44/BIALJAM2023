d96c9fffde5d409bd6889d3bec2154ff4075a8ca
michal.kow98 <michal.kow98@interia.pl>
2023-06-09 19:36:56 +0200

    fix

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -5,19 +5,15 @@ public class Car
 {
     public int TypeId;
     public int portrait;
-    public Person person;
+    public Licence person;
     //public Color color;
     public string carNum;
     public int[] carComponenetsl;
 
-    public Car(int typeId, Person person, string carNum, int[] carComponenetsl)
+    public Car(int typeId, Licence person, string carNum, int[] carComponenetsl)
     {
         TypeId = typeId;
-<<<<<<< HEAD
-        this.person = Person.GeneratePerson();
-=======
-        this.person = Person.GeneratePerson(); ;
->>>>>>> 58b7fb63f00969498b8b0cf04e57837d26ae07c7
+        this.person = Licence.GenerateLicence();
         this.carNum = carNum;
         this.carComponenetsl = InitCarComponents();
     }
